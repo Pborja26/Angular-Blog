e39b2ffaf2f56b09c0b91094aad4621404a564f0
Pedro <pedrorborja@gmail.com>
2024-02-02 17:09:21 -0300

    Components created with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
+import { BigCardComponent } from './big-card/big-card.component';
+import { SmalCardComponent } from './smal-card/smal-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmalCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
